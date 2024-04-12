d07e45968c07470e25029965ce1d2d47eeb2a622
nirajacharyaa <mail.nirajacharya@gmail.com>
2024-04-12 21:13:46 +0545

    feat: :sparkles: handled error and loading states

--- src/AllNews.tsx
+++ src/AllNews.tsx
@@ -8,8 +8,80 @@ const AllNews = () => {
     queryKey: ["news"],
     queryFn: extractNewsData,
   });
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error.message}</div>;
+  if (isLoading)
+    return (
+      <div className="w-full max-w-[32rem] mx-auto min-h-screen flex flex-col items-center justify-center flex-1 shrink-0">
+        <div className=" h-1/3 bg-gray-200 dark:bg-gray-800 animate-pulse flex items-center justify-center">
+          <div>
+            <svg
+              aria-hidden="true"
+              className="w-8 h-8 text-gray-400 animate-spin dark:text-gray-600 fill-black"
+              viewBox="0 0 100 101"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor"
+              />
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill"
+              />
+            </svg>
+            <span className="sr-only">Loading...</span>
+          </div>
+        </div>
+        <div className="px-4">
+          <div className="mt-4 h-4 w-40 bg-gray-200 dark:bg-gray-800 animate-pulse"></div>
+          <div className="mt-10 h-8 w-full bg-gray-200 dark:bg-gray-800 animate-pulse"></div>
+
+          <div className="mt-12">
+            {[...Array(4)].map((_, index) => (
+              <div
+                key={index}
+                className="my-1 h-4 w-full bg-gray-200 dark:bg-gray-800 animate-pulse"
+              ></div>
+            ))}
+            <div className="my-1 h-4 w-1/2 bg-gray-200 dark:bg-gray-800 animate-pulse"></div>
+          </div>
+          <div className="mt-6 h-6 w-32 bg-gray-200 dark:bg-gray-800 animate-pulse"></div>
+          <div className=" mt-6 flex items-center gap-4">
+            {[...Array(3)].map((_, index) => (
+              <div
+                key={index}
+                className="my-1 h-12 w-12 rounded-full bg-gray-200 dark:bg-gray-800 animate-pulse"
+              ></div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+
+  if (error)
+    return (
+      <div className="w-full max-w-[32rem] mx-auto min-h-screen flex flex-col flex-1 shrink-0">
+        {/* show a cross svg with message as error fetching data */}
+        <div className="flex items-center justify-center h-full">
+          <div className="flex flex-col items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="56"
+              height="56"
+              className="fill-rose-500"
+              viewBox="0 0 16 16"
+            >
+              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z" />
+            </svg>
+
+            <p className="mt-4 text-lg font-semibold text-gray-700 dark:text-gray-300">
+              An error occurred
+            </p>
+            <span className="text-sm text-gray-500">Check later</span>
+          </div>
+        </div>
+      </div>
+    );
 
   console.log(data);
   return (
