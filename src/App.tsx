5978e9ff39ea66eb0cbe2d75f2be86046061108f
Bikram Parajuli <bikramparajuli0000@gmail.com>
2024-04-12 14:52:48 +0545

    dark mode toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AllNews from "./AllNews";
+import { ThemeProvider } from "./components/theme-provider";
+import { ModeToggle } from "./components/toggle";
 
 const queryClient = new QueryClient();
 
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <main className="flex flex-col min-h-screen snap-y snap-mandatory">
-        <AllNews />
-      </main>
+      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+        <main className="flex flex-col min-h-screen snap-y snap-mandatory">
+          <AllNews />
+          <ModeToggle />
+        </main>
+      </ThemeProvider>
     </QueryClientProvider>
   );
 }
