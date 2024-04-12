3f52a13b7538b116992c7f8f1876288bac0bd3bd
Bikram Parajuli <bikramparajuli0000@gmail.com>
2024-04-12 14:54:10 +0545

    Added Dark Mode Toggle

--- src/components/toggle.tsx
+++ src/components/toggle.tsx
@@ -15,7 +15,11 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
+        <Button
+          variant="outline"
+          size="icon"
+          className="fixed bottom-4 right-4"
+        >
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
